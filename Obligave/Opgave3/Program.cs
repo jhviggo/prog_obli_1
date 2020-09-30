73d23d4d1e06b4ddf863c967756ebf351451bd67
Andreas Jensen <Andreasvejlstrup@hotmail.com>
2020-09-30 22:58:00 +0200

    Henries

---
+++ Obligave/Opgave3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Obligave.Opgave3
+{
+    class ProgramOpgave3
+    {
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        public static void Run()
+        {
+            Console.WriteLine("Generating Henry...");
+            stopwatch.Start();
+            for (int i = 0; i < 10000000000; i++) 
+            {
+                Henry henry = new Henry();
+            }
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            Console.WriteLine("Henry generated");
+            Console.WriteLine(ts.Hours + ":" + ts.Minutes + ":" + ts.Seconds + ":" + ts.Milliseconds);
+
+        }
+    }
+}
--- Obligave/Program.cs
+++ Obligave/Program.cs
@@ -1,3 +1,4 @@
+using Obligave.Opgave3;
 using System;
 
 namespace Obligave
@@ -13,7 +14,13 @@ namespace Obligave
 
             // Opgave 2
             Console.WriteLine("## Opgave 2 ##");
-            ProgramOpgave2.Run(10000, 20); // 10k strings of length 20
+            //ProgramOpgave2.Run(10000, 20); // 10k strings of length 20
+            Console.WriteLine();
+
+
+            // Opgave 3
+            Console.WriteLine("## Opgave 3 ##");
+            ProgramOpgave3.Run();
             Console.WriteLine();
         }
     }
