14702373835a82f98512e284330425890bee95e4
Andreas Jensen <Andreasvejlstrup@hotmail.com>
2020-09-30 14:33:59 +0200

    init commit suicide

---
+++ Obligave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Obligave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
